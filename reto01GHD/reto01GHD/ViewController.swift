c3f5e75d0ace861fddff0dd4255d7ebd863d6fba
Joshua Sosa Cruz <joshua.developer21@gmail.com>
2023-04-17 09:28:15 -0500

    add constraints

---
+++ reto01GHD/reto01GHD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  reto01GHD
+//
+//  Created by user239944 on 4/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "Pokedex"
+    }
+
+
+}
+
